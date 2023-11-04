417522ee96f7b64739f2a705b220f1cff717174c
santhosh-kumar-17 <santhoshpunk18@gmail.com>
2023-11-04 20:15:27 +0530

    before useStates

---
+++ frontend/data-visualization/src/components/Contactcard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Auth from "../images/Auth.png";
+
+const Contactcard = (props)=>{
+    const {id,name,email} = props.contacts;
+    return(
+        <div className="item">
+            <img className="ui avatar image" src={Auth} alt="images" />
+                <div className="content">
+                    <div className="header">
+                        <div>
+                            {name} 
+                        </div>
+                        <div>{email}</div>
+                    </div>
+                    <i className="trash alternate outline icon" style={{color:"red",marginTop:"7px"}}></i>
+                    {/* to add inline css we have to use {} curly braces which is render
+                    in myg assumption and also another {} to render css properties 
+                    and if we need to use margin-top we have to use marginTop camelCase 
+                    to use it  */}
+                </div>
+            </div>
+    )
+}
+
+export default Contactcard;
--- frontend/data-visualization/src/components/Contactlist.js
+++ frontend/data-visualization/src/components/Contactlist.js
@@ -1,20 +1,11 @@
 import React from "react";
+import Contactcard from "./Contactcard";
 
 const Contactlist= (props)=>{
     console.log(props)
     const renderContactList = props.contacts.map((contacts)=>{
         return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">
-                        <div>
-                            {contacts.name} 
-                        </div>
-                        <div>{contacts.email}</div>
-                    </div>
-                    <i className="trash alternate outline icon"></i>
-                </div>
-            </div>
+            <Contactcard contacts={contacts} />
         )
     })
 
