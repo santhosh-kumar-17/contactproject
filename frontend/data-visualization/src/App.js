0ae874c9cee0cfc0ec3e9bc56dfbbde0a50ac4c2
santhosh-kumar-17 <santhoshpunk18@gmail.com>
2023-11-06 00:35:54 +0530

    till useState  pakka!!

--- frontend/data-visualization/src/App.js
+++ frontend/data-visualization/src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import './App.css';
-import Header from "./components/Header";
-import Addcontact from './components/Addcontact';
-import Contactlist from "./components/Contactlist";
+  import React, { useState, useEffect } from 'react';
+  import './App.css';
+  import Header from "./components/Header";
+  import Addcontact from './components/Addcontact';
+  import Contactlist from "./components/Contactlist"
 
+  function App() {
+    const LOCAL_STORAGE_KEY="contacts";
+    const [contacts, setContact] = useState([]);
 
+    const addContactHandler =(contact)=>{
+      console.log(contact.name);
+      setContact([...contacts,contact]);
+    }
 
-function App() {
-  const contacts = [
-    {
-      id : "1",
-      "name": "santhosh",
-      "email":"punk18@gmail.com"
-    },
-    {
-      id : "2",
-      "name": "syed",
-      "email":"syedpunk18@gmail.com"
-    },
-  ];
-  return (
-    <div className='ui container'>
-      <Header />
-      <Addcontact />
-      <Contactlist contacts={contacts} />
-    </div>
-  );
-}
+    useEffect(()=>{
+      const retrivedContact= JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      if(retrivedContact){
+        setContact(retrivedContact)
+      } 
+    },[]);
+    
+    useEffect(()=>{
+      localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts)); 
+    },[contacts]);
+    
+    return (
+      <div className='ui container'>
+        <Header />
+        <Addcontact addContactHandler={addContactHandler} />
+        <Contactlist contacts={contacts} />
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
--- frontend/data-visualization/src/components/Addcontact.js
+++ frontend/data-visualization/src/components/Addcontact.js
@@ -1,20 +1,34 @@
 import React from "react";
 
 class Addcontact extends React.Component {
+    state={
+        name : "",
+        email : "",
+    };
+
+    add = (e)=>{
+        e.preventDefault();
+        if(this.state.name === "" || this.state.email === ""){
+            alert("all fields are mandatory");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({name : "" ,email :""})
+     }
     render() {
         return (
             <div className="ui main">
                 <h2>add contact</h2> 
                 <h2>Add contact</h2> 
                 
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="name"  placeholder="Name" onChange={(e)=> this.setState({name : e.target.value})} />
                     </div>
                     <div className="field">
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="Email" />
+                        <input type="text" name="email"  placeholder="Email" onChange={(e)=> this.setState({email : e.target.value})} />
                     </div>
                     <button className="ui button blue">add</button>
                 </form>
